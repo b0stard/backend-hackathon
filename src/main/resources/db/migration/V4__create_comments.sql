a26b89c18a1bc862addc05fef7a8314fee8c5fbf
dippe <sostanin378@gmail.com>
2026-04-14 21:03:21 +0300

    tutu2123

--- src/main/resources/db/migration/V4__create_comments.sql
+++ src/main/resources/db/migration/V4__create_comments.sql
@@ -1,1 +1,7 @@
-V4__create_comments.sql
+create table if not exists comments (
+                                        id bigserial primary key,
+                                        task_id bigint not null references tasks(id) on delete cascade,
+    author_id bigint not null references users(id) on delete restrict,
+    text text not null,
+    created_at timestamp not null default now()
+    );
