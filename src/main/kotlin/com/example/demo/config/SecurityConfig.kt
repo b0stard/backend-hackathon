9e6a1df23b4b1714041e781f787c1390e11a8c21
dippe <sostanin378@gmail.com>
2026-04-16 23:54:14 +0300

    tutu2123

--- src/main/kotlin/com/example/demo/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/config/SecurityConfig.kt
@@ -2,20 +2,22 @@ package com.example.demo.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-@EnableWebSecurity
 class SecurityConfig {
 
     @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
+            .cors { }
             .csrf { it.disable() }
             .authorizeHttpRequests {
-                it.anyRequest().permitAll()
+                it
+                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                    .anyRequest().permitAll()
             }
 
         return http.build()
