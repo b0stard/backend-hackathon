56317a5f988461eb33d30c921e70e9df88ac0e76
dippe <sostanin378@gmail.com>
2026-04-16 23:43:18 +0300

    tutu2123

--- src/main/kotlin/com/example/demo/config/WebConfig.kt
+++ src/main/kotlin/com/example/demo/config/WebConfig.kt
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
     }
