e216e98172078b40fce888099ac0b9599297394a
b0stard <155761442+b0stard@users.noreply.github.com>
2026-04-17 13:32:37 +0300

    Update WebConfig.kt

--- src/main/kotlin/com/example/demo/config/WebConfig.kt
+++ src/main/kotlin/com/example/demo/config/WebConfig.kt
@@ -1,18 +1,17 @@
 package com.example.demo.config
 
-
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("*")
+            .allowedOrigins("http://localhost:5173") 
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
-            .allowCredentials(true)
+            .allowCredentials(true) 
     }
 }
